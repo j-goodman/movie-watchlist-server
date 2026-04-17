3b4ff9c5e92ba3490c66e2a623cb28f6b5d84d47
j-goodman <johncurtisgoodman@gmail.com>
2026-04-16 20:59:02 -0400

    Add actor lookup route and listen for post requests.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import movies from './movies.js'
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.json({
@@ -15,6 +16,25 @@ app.get("/movies", (req, res) => {
 
 app.get("/movies/:name", (req, res) => {
     res.json(movies.find(movie => movie.title === req.params.name))
+})
+
+app.get("/movies/actor/:actorName", (req, res) => {
+    res.json(movies.filter(movie => movie.starring.includes(req.params.actorName)))
+})
+
+let counter = 20
+
+app.post("/movies", (req, res) => {
+    const newMovie = {
+        title: req.body.title,
+        starring: req.body.starring,
+        year: req.body.year,
+        watched: false,
+        id: counter++
+    }
+
+    movies.push(newMovie)
+    res.status(201).json(newMovie)
 })
 
 const port = 3000
